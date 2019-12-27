7aed22fbe850835195aa76f459b4fcdd7ea18eea
tomkotlar <t.kotlar@yahoo.com>
2019-12-27 11:53:17 +0000

    adding simple routing to hompage and hatspage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React from "react"
+import "./App.css"
+import { Route, Switch } from "react-router-dom"
 
-import './App.css';
-import HomePage from './Pages/HomePage';
+import HomePage from "./Pages/HomePage"
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>hats</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-     
-<HomePage />
+      <Switch> 
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
