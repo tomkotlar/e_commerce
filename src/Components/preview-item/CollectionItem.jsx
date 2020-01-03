6072cc893ed69c6efa1cc73ac26bb93468cacfa2
tomkotlar <t.kotlar@yahoo.com>
2020-01-03 18:45:51 +0000

    adding styling to custom button in shop collection commponent

--- src/Components/preview-item/CollectionItem.jsx
+++ src/Components/preview-item/CollectionItem.jsx
@@ -1,13 +1,15 @@
 import React from "react"
-import "./CollectionItem.styles.scss" 
+import "./CollectionItem.styles.scss"
+import CustomButton from "../custom-button/CustomButton"
 
-const CollectionItem = ({ id, name, price, imageUrl}) => (
+const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item ">
     <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
     <div className="collection-footer">
       <span className="name">{name}</span>
       <span className="price">${price}</span>
     </div>
+      <CustomButton inverted> Add to cart </CustomButton>
   </div>
 )
 
