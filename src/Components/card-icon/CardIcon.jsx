25414122f4b819d0978ec8251798a027d79d932e
tomkotlar <t.kotlar@yahoo.com>
2020-01-04 16:19:40 +0000

    adding funcionality/reducer to cardIcon component

--- src/Components/card-icon/CardIcon.jsx
+++ src/Components/card-icon/CardIcon.jsx
@@ -1,21 +1,26 @@
 import React from "react"
 import { ReactComponent as ShoppingIcon } from "../../graphic_assets/shopping-bag.svg"
 import "./CardIcon.styles.scss"
-import { toggleCartHidden } from "../../redux/cart/cart.action";
-import { connect } from "react-redux";
+import { toggleCartHidden } from "../../redux/cart/cart.action"
+import { connect } from "react-redux"
 
 
-
-const CardIcon = ({toggleCartHidden}) => {
+const CardIcon = ({ toggleCartHidden, itemCount }) => {
   return (
-    <div className="cart-icon" onClick={toggleCartHidden} >
+    <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   )
 }
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 })
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (acumulatedQuantity, cartItem) => 
+    acumulatedQuantity + cartItem.quantity, 0
+  )
+})
 
-export default connect(null, mapDispatchToProps)(CardIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CardIcon)
