64c43e2d35f2a35712ca61627dab8c1462c3dc84
tomkotlar <t.kotlar@yahoo.com>
2020-01-07 13:54:38 +0000

    adding remove form checkOutPage func.

--- src/Components/checkout-item/CheckOutItem.jsx
+++ src/Components/checkout-item/CheckOutItem.jsx
@@ -1,7 +1,10 @@
 import React from "react"
 import "./CheckOutItem.styles.scss"
+import { clearItemFromCart } from "../../redux/cart/cart.action"
+import { connect } from "react-redux"
 
-const CheckOutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
+const CheckOutItem = ({ cartItem, clearItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem
   return (
     <div className="checkout-item">
       <div className="image-container">
@@ -10,9 +13,17 @@ const CheckOutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
       <span className="name">{name}</span>
       <span className="quantity">{quantity}</span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div 
+      onClick={() => clearItem(cartItem)} 
+      className="remove-button">
+        &#10005;
+      </div>
     </div>
   )
 }
 
-export default CheckOutItem
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckOutItem)
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -12,15 +12,22 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         hidden: !state.hidden
-      };
+      }
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
-      };
+      }
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+      }
     default:
-      return state;
+      return state
   }
 };
 
-export default cartReducer;
+export default cartReducer
