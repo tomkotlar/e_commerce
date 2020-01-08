6405d9fcc5896cec4d73d0221d6b0412d6dd7045
tomkotlar <t.kotlar@yahoo.com>
2020-01-08 14:00:32 +0000

    adding directory reducer/ moving data from directory component to reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,17 +4,18 @@ import storage from "redux-persist/lib/storage"
 
 import userReducer from "./user/user.reducer"
 import cartReducer from "./cart/cart.reducer"
+import directoryReducer from "./directory/directory.reducer"
 
 const persistConfig = {
-    key: 'root',
-    storage, 
-    whitelist: ['cart']
+  key: "root",
+  storage,
+  whitelist: ["cart"]
 }
 
-
-const rootReducer =  combineReducers({
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 })
 
-export default persistReducer(persistConfig ,rootReducer)
+export default persistReducer(persistConfig, rootReducer)
